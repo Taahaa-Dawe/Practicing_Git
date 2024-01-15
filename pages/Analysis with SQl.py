214fb538f77814f37eeb4d1c96f239e8bb3841f5
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-01-14 19:16:06 -0700

    Update Analysis with SQl.py

--- pages/Analysis with SQl.py
+++ pages/Analysis with SQl.py
@@ -1,19 +1,5 @@
 import streamlit as st
 
-
-conn = st.connection('mysql', type='sql')
-try:
-
-    st.write(conn)
-    db_name = st.text_input("enter database name ")
-    if st.button("Create Database"):
-        sql = "create database " +db_name
-        conn._instance.execute(sql)
-        st.write("database created")
-
-except Exception as e:
-	st.write("issue ", e)
-
-st.write("python with sql anlysis")
+st.write("DB username:", st.secrets[connections.mysql]["host"])
 
 
