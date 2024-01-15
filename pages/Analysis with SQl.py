6d4d062e920eb1576c82b414e723eb1fc4187b44
Taahaa-Dawe <145599886+Taahaa-Dawe@users.noreply.github.com>
2024-01-14 20:20:41 -0700

    Update Analysis with SQl.py

--- pages/Analysis with SQl.py
+++ pages/Analysis with SQl.py
@@ -1,23 +1,7 @@
 import streamlit as st
-from sqlalchemy import create_engine
 
-# Connect to the database
-engine = create_engine("mysql+mysqlconnector://root:abc123@localhost:3306")
 
-try:
-    st.write(engine)
-    
-    db_name = st.text_input("Enter database name ")
-    
-    if st.button("Create Database"):
-        # Create the database
-        with engine.connect() as connection:
-            connection.execute(f"CREATE DATABASE {db_name}")
-        st.write("Database created")
-
-except Exception as e:
-    st.write("Issue: ", e)
-
+conn = st.connection('mysql', type='sql')
 st.write("Python with SQL analysis")
 
 
